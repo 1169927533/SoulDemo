47fe29b2ad4dfe104c16f9f1382faa1b9611607a
YuZQ <1169927533@qq.com>
2020-07-31 14:41:17 +0800

    'debug'

---
+++ README.md
@@ -0,0 +1,11 @@
+# Soul Planet
+
+Soul App星球效果
+
+### 效果图
+
+<img src="https://github.com/HaowenLee/SoulPlanet/blob/master/arts/soul_planet.gif" width="360" alt="SoulPlanet效果图"/>
+
+### 感谢
+
+基于[misakuo/3dTagCloudAndroid](https://github.com/misakuo/3dTagCloudAndroid)，修改部分代码实现
---
+++ app/src/main/java/me/haowen/sample/MainActivity.java
@@ -0,0 +1,19 @@
+package me.haowen.sample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.flContainer, new MainFragment())
+                .commitAllowingStateLoss();
+    }
+}
